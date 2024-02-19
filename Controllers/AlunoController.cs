538794605887ea0b75789621ec4a7157347ba9af
Guilherme Costa <costa.gguilherme02@gmail.com>
2024-02-19 10:12:12 -0300

    feat: Metodos controller de Aluno

--- Controllers/AlunoController.cs
+++ Controllers/AlunoController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using SmartSchool.WebAPI.Models;
 
 namespace SmartSchool.WebAPI.Controllers
 {
@@ -10,10 +11,51 @@ namespace SmartSchool.WebAPI.Controllers
         {
         }
 
+        public List<Aluno> Alunos = new List<Aluno> {
+            new Aluno() {
+                Id = 1,
+                Nome = "Guilherme",
+                Sobrenome = "Lopes",
+                Telefone = "71988232332"
+            },
+                        new Aluno() {
+                Id = 2,
+                Nome = "Marta",
+                Sobrenome = "Medeiros",
+                Telefone = "85991458930"
+            }
+        };
+
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok("Alunos: Marta, Paula, Lucas, Rafa");
+            return Ok(this.Alunos);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var aluno = Alunos.FirstOrDefault(a => a.Id == id);
+            if (aluno == null) return BadRequest("Aluno não encontrado");
+            return Ok(aluno);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
         }
     }
 }
